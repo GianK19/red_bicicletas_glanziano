b37dc96d5da33c15ccd4f91889cd9231066a1e02
GianK19 <glanziano@unal.edu.co>
2026-03-01 17:56:47 -0500

    agrgar mapa y marcadores

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,10 @@
-var map = L.map('main_map').setView([4.60971, -74.08175], 13);
+var map = L.map('main_map').setView([4.6481867, -74.1341518], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([4.60971, -74.08245]).addTo(map)
-L.marker([4.60971, -74.08455]).addTo(map)
-L.marker([4.60971, -74.08105]).addTo(map)
+L.marker([4.6963414, -74.1111205]).addTo(map)
+L.marker([4.710703, -74.1145326]).addTo(map)
+L.marker([4.6966124, -74.1499631]).addTo(map)
